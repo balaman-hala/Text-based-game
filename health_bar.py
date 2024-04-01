56482c19f1ca8b57d7e318531324458c5c23d516
balaman-hala <155171535+balaman-hala@users.noreply.github.com>
2024-04-01 17:51:04 +0100

    update

--- health_bar.py
+++ health_bar.py
@@ -1,7 +1,9 @@
 import os
+import curses
 from colorama import Fore, Back, Style, init
-os.system("")
 
+os.system("")
+init()  # Initialize colorama
 
 class HealthBar:
     symbol_remaining: str = "▮"
@@ -24,11 +26,24 @@ class HealthBar:
                     "gold": Fore.YELLOW,
                     "default": Fore.RESET}
 
-    def __init__(self, entity, length, current_value, is_colored, color) -> None:
+    def __init__(self, entity, length: int = 20, is_colored: bool = True, color: str = "") -> None:
         self.entity = entity
         self.length = length
         self.max_value = entity.health_max
         self.current_value = entity.health
         self.is_colored = is_colored
-        self.color = color
-        
+        self.color = self.colors.get(color) or self.colors["default"]
+
+    def update(self) -> None:
+        self.current_value = self.entity.hp
+
+    def draw(self, stdscr) -> None:
+        remaining_bars = round(self.current_value / self.max_value * self.length)
+        lost_bars = self.length - remaining_bars
+        stdscr.addstr(f"{self.entity.name}'s Health: {self.entity.health}/{self.entity.health_max}\n")
+        stdscr.addstr(f"{self.barrier}"
+                      f"{self.color if self.is_colored else ''}"
+                      f"{self.symbol_remaining * remaining_bars}"
+                      f"{self.symbol_lost * lost_bars}"
+                      f"{self.colors['default'] if self.is_colored else ''}"
+                      f"{self.barrier}\n")
